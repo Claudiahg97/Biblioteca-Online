d580a4a955ebb5c7d556f7b347675a8a66e597a2
Claudiahg97 <claudia.hg.97@gmail.com>
2025-10-03 18:05:13 +0100

    cambio en el indice

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <form action="php/registro.php" method = "POST">
     <h3>Registro de nuevo usuario</h3>        
     Nombre: <input type="text" name="name" value = "<?php if(isset($_SESSION['nombre'])) echo $_SESSION['nombre']?>"> <br>   
-    Email: <input type="text" name="email" value = "<?php  if(isset($_SESSION['email'])) echo $_SESSION['email']?>"><br>       
+    Email: <input type="text" name="email" placeholder= "ejmplo@mail.com" value = "<?php  if(isset($_SESSION['email'])) echo $_SESSION['email']?>"><br>       
     Contraseña: <input type="password" name="password" required > <br>
     Repite la contraseña: <input type="password" name="compPassword" required> <br>
     <input type="submit" name= "abrir" value="Registrarse" >
@@ -18,7 +18,7 @@
     
     <form action="php/inicio.php" method = "POST">
     <h3>Inicio de Sesión</h3>
-    Email: <input type="text" name="emailI" value = "<?php  if(isset($_SESSION['emailI'])) echo $_SESSION['emailI']?>"><br>       
+    Email: <input type="text" name="emailI" placeholder= "ejmplo@mail.com" value = "<?php  if(isset($_SESSION['emailI'])) echo $_SESSION['emailI']?>"><br>       
     Contraseña: <input type="password" name="contra" required > <br>
     <input type="submit" name= "abrir" value="Iniciar Sesión" >
     </form>
